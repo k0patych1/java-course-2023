7ba6e35dea00e812fcd28e9d0018b617e972971a
k0patych1 <367512@niuitmo.ru>
2023-10-29 22:29:06 +0300

    debug comparator in task6 to desc

--- src/main/java/edu/hw3/task6/StockComparator.java
+++ src/main/java/edu/hw3/task6/StockComparator.java
@@ -5,6 +5,6 @@ import java.util.Comparator;
 public class StockComparator implements Comparator<Stock> {
     @Override
     public int compare(Stock stock1, Stock stock2) {
-        return stock1.price().compareTo(stock2.price());
+        return stock2.price().compareTo(stock1.price());
     }
 }
